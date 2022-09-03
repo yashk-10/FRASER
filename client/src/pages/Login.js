2540e712fa6e23c93a70783609f020fc4c733dda
yashk-10 <62525812+yashk-10@users.noreply.github.com>
2022-09-03 18:04:52 -0400

    login page 40% complete

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Flex, Heading, Text, Input, Button } from '@chakra-ui/react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Flex height="100vh" alignItems="center" justifyContent="center" background="#DADFE2">
+      <Flex display="flex" direction="row" background="#C2D3DD" borderRadius="10px">
+        <Flex direction="column" alignItems="center">
+          <Heading mb={5}>Welcome Back</Heading>
+          <Text fontSize="md">Enter your credentials and we’ll get started with the verification process.</Text>
+          <Input placeholder="Username" variant="filled" mb={3} type="email"/>
+          <Input placeholder="Password" variant="filled" mb={6} type="password"/>
+          <Button colorScheme="navy"
+
+        </Flex>
+        <Image src="" alt="Login Image"/>
+      </Flex>
+    </Flex>
   )
 }
 
