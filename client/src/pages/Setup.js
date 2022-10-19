85d60c3334a2d04a821eed24610c28a8f7c9ebc9
yashk-10 <62525812+yashk-10@users.noreply.github.com>
2022-10-18 23:25:43 -0400

    popover implemented

--- client/src/pages/Setup.js
+++ client/src/pages/Setup.js
@@ -1,13 +1,17 @@
 import React, {useRef, useEffect, useState} from 'react'
-import { Flex, Heading, Text, Input, Button, Image } from '@chakra-ui/react'
+import { Flex, Heading, Text, Input,
+         Button, Image , Popover, PopoverTrigger,
+          PopoverContent, PopoverArrow, PopoverCloseButton, PopoverHeader,
+        PopoverBody, } from '@chakra-ui/react'
 
 const Setup = () => {
     const cameraRef = useRef(null);
     const imageRef = useRef(null);
 
     const [button, setButton] = useState("Capture")
-    const [display, setDisplay] = useState("block")
-    const [photoExists, setPhotoExists] = useState(false);
+    // const [display, setDisplay] = useState("block")
+    // const [display2, setDisplay2] = useState("none")
+    // const [photoExists, setPhotoExists] = useState(false);
 
     const startVideo = () => {
         navigator.mediaDevices
@@ -30,13 +34,14 @@ const Setup = () => {
         let image = imageRef.current;
 
 
-        image.width = 465;
-        image.height = 564;
+        // image.width = 465;
+        // image.height = 564;
+        image.width = 300
+        image.height = 375
 
         let ctx = image.getContext('2d')
-        ctx.drawImage(camera, 0, 0, 465, 564);
+        ctx.drawImage(camera, 0, 0, 300, 375);
         setButton("Submit")
-        setDisplay("none")
     }
 
 
@@ -49,10 +54,24 @@ const Setup = () => {
         <Flex height="39.6vw"  display="flex" direction="row" background="#ACBCCB" borderRadius="10px">
             
             <Flex width="80vw" m="3vw"  justifyContent="center">
-                <video style={{display: display}} ref={cameraRef} ></video>
-                <canvas ref={imageRef}></canvas>
-                <Button onClick={captureImage}>{button}</Button>
+                <video ref={cameraRef} ></video>
+                <Popover >
+                <PopoverTrigger>
+                    <Button onClick={captureImage}>{button}</Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverCloseButton />
+                    <PopoverHeader>Success!</PopoverHeader>
+                    <PopoverBody >
+                        <canvas ref={imageRef}/>
+                        <Button>Submit</Button>
+                    </PopoverBody>
+                </PopoverContent>
+                </Popover>
+                {/* <Button onClick={captureImage}>{button}</Button> */}
             </Flex>
+            {/* <canvas style={{display2: display2}} ref={imageRef}/> */}
             
         </Flex>
         </Flex>
